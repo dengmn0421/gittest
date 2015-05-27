beb4771db6f8cfa398bd31c638b6b620d0f4d819
dengmn0421 <dengmn0421@thundersoft.com>
2015-05-27 16:32:23 +0800

    add some output

--- readme.c
+++ readme.c
@@ -2,7 +2,9 @@
 int main (void)
 {
 	printf("先写个压压惊\n");
-	printf("hello world\n");
-	printf("再写一行来测试下～\n");
+	printf("hello world~\n");
+	printf("开始来测试下～\n");
+    printf("随便写点吧\n");
+    printf("再写一句吧\n");
 	return 0;
 }
